d9facff38763577f3d267ac96041b445e8f68b38
Ron Martin <ronmartin89@gmail.com>
2020-01-16 11:35:24 -0800

    Exercise 5 done

---
+++ src/api.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/Ron';
+
+function getItems() {
+  return fetch(`${BASE_URL}/items`);
+}
+
+function createItem(name) {
+  const newItem = JSON.stringify({ name });
+  return fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: newItem
+  });
+}
+
+export default {
+  getItems,
+  createItem
+};
--- src/index.js
+++ src/index.js
@@ -4,10 +4,42 @@ import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
+import api from './api';
+import store from './store';
 
 const main = function () {
+
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
+
+    
   shoppingList.bindEventListeners();
   shoppingList.render();
+
+
+  /* Even though thiscall runs first, since its Asynchronous, it actually
+   gets output after the api.getItems() call at the bottom of the page (not the one inside this then block).
+   This is because they are both Asynchronous, and the api.getItems(); call (ONE GET) gets resolved faster 
+   than our api.createItems(api.getItems) which is a POST and a GET */
+  api.createItem('pears')
+    .then(res => res.json())
+    .then((newItem) => {
+      return api.getItems();
+    })
+    .then(res => res.json())
+    .then((items) => {
+      console.log(items);
+    });
+
+  // api.getItems()
+  //   .then(res => res.json())
+  //   .then(res => console.log(`GET ITEMS ${res}`));
 };
+
+
 
 $(main);
